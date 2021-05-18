938c4bcc297299eb6ed0a3c6cd0e8055ca136919
Grecia Carolina Coronado <coronadogrecia.15@gmail.com>
2021-05-17 23:26:46 -0500

    codigo correcto

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+    FORMATO DE CADA TAREA
+        - titulo: string
+        - status: string (
+           * terminada
+           * pendiente
+         )
+*/
+let nueva = []
+exports.leerTareas = function () {
+    for (let i=0;i< nueva.lenght; i++){
+        let nuev= nueva.push(nueva[i]);
+    }
+    return nueva
+}
+exports.agregarTarea = function (tarea) {
+    
+    let count = nueva.push(tarea);
+    return nueva
+}
+exports.filtrarTareasPorEstado = function (estado) {
+    let filtro= nueva.filter(function(el){
+        return el.status == estado;
+    })
+    return filtro
+}
+
